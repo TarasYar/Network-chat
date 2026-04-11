2751d8f6ba666e185b41ecd0644e305c9fc67b9c
Taras <tarasyaremko14@gmail.com>
2026-04-11 13:11:52 +0300

    Next project

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /media/taras/Kurs/Курси/CppSystem Developer/portfolio/project
+# Build directory: /media/taras/Kurs/Курси/CppSystem Developer/portfolio/project/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
